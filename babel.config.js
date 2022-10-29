963097ea57639d93370f467f2fe310e06025dda2
naffeti-houaida <80219141+naffeti-houaida@users.noreply.github.com>
2022-10-29 10:17:16 +0100

    petite changement

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,6 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  devServer: {
-    host: '0.0.0.0',
-    https: false,
-    port: 8080,
-    public: 'http://0.0.0.0:8080'
-  },
+
 
 }
